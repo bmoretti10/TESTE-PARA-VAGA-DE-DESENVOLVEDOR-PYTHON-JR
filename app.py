2b8deb4b9c69ffc3de22bb7bec140f8adb3102d2
bmoretti10 <143190091+bmoretti10@users.noreply.github.com>
2025-02-12 21:39:25 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/saudacao', methods=['GET'])
+def saudacao():
+    nome = request.args.get('name')
+    if nome:
+        return f"Olá, {nome}!"
+    else:
+        return "Olá, Camila Schaustz!"
+
+@app.route('/soma', methods=['POST'])
+def soma():
+    try:
+        data = request.get_json()
+        num1 = data['num1']
+        num2 = data['num2']
+        resultado = num1 + num2
+        return jsonify({'soma': resultado})
+    except (TypeError, KeyError):
+        return jsonify({'erro': 'Parâmetros inválidos'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
